4b10ad3847d37d2fd97f36317dd0df3467e6700b
shsazib <shsazibbhuiyan892939@gmail.com>
2022-09-30 23:41:29 -0700

    api-data-implement

---
+++ src/Component/Profile.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import "./Profile.css";
+import axios from "axios";
+
+const Data = () => {
+  const [profileName, setProfileName] = useState("");
+  const [profileCell, setProfileCell] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [profileEmail, setProfileEmail] = useState("");
+
+  const profileData = async () => {
+    try {
+      const res = await axios.get("https://randomuser.me/api/");
+      setProfileCell(res.data.results[0].cell);
+      setProfileEmail(res.data.results[0].email);
+      setProfileImage(res.data.results[0].picture.large);
+      setProfileName(
+        `${res.data.results[0].name.first} ${res.data.results[0].name.last}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    profileData();
+  }, []);
+
+  return (
+    <>
+      <div className="profile">
+        <button onClick={() => profileData()}>New Person</button>
+        <div className="card">
+          <img src={profileImage} alt="img" style={{ width: "20%" }} />
+          <h1>{profileName}</h1>
+          <p className="title">{profileEmail}</p>
+          <p>{profileCell}</p>
+          <p>
+            <button>Contact</button>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Data;
